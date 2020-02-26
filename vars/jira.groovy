a369de12eea85626bc20b85431440c06fdbc3251
danlix2000 <51103503+danlix2000@users.noreply.github.com>
2020-02-26 11:43:50 +0530

    gggg

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -4,7 +4,7 @@ testIssue = [fields: [ project: [id: '10000'],
                            summary: 'New JIRA Created from Jenkins.',
                            description: 'New JIRA Created from Jenkins.',
                            issuetype: [name: 'Task' ]]]
-
+response = jiraNewIssue issue: testIssue, site: 'jiradd'
 
 echo response.successful.toString()
 echo response.data.toString()
